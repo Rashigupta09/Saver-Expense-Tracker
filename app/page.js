437911cdfd7f230164cae0d9858d708e57f84c12
Rashi-Gupta <rashigupta0906@gmail.com>
2025-08-09 00:52:12 +0530

    Full and Final Landing Page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 import HeroSection from "@/components/hero";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, } from "@/components/ui/card";
 import { featuresData, howItWorksData, statsData, testimonialsData } from "@/data/landing";
 import Image from "next/image";
+import Link from "next/link";
 
 
 export default function Home() {
@@ -82,6 +84,19 @@ export default function Home() {
             ))}
           </div>
         </div>
+      </section>
+
+      <section className="py-10 bg-blue-700">
+        <div className="container mx-auto px-4 text-center">
+          <h2 className="text-3xl font-bold text-white mb-4">Ready to Take Control of Your Finances</h2>
+          <p className="text-blue-100 mb-8 max-w-2xl mx-auto">Join thousands of users who already managing their finances smarter with Saver</p> 
+          <Link href="\dashboard">
+            <Button size="lg" className="bg-white text-blue-600 hover:bg-blue-50 animate-bounce">
+              Start Free Trial
+          
+            </Button>
+          </Link> 
+          </div>
       </section>
     </div>
   );
