b8493d014df17e50c88c069a260c86fd69b67b41
Rashi-Gupta <rashigupta0906@gmail.com>
2025-08-16 12:47:15 +0530

    create account functionality added

--- components/header.jsx
+++ components/header.jsx
@@ -10,8 +10,11 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { LayoutDashboard, PenBox } from "lucide-react";
+import { checkUser } from "@/lib/checkUser";
 
-const Header = () => {
+const Header =  async () => {
+  await checkUser();
+
   return (
     <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
       <nav className="container mx-auto px-2 py-1.3 flex justify-between items-center">
